2846ec61857f8fabf7561cdbf527827187dc1d4b
Samiaislam1 <samia.10.sanam@gmail.com>
2022-03-04 10:14:24 +0600

    www

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const calculate1 = () => {
+const calculate = () => {
+
     const firstInput = document.getElementById('1st');
     const firstText = firstInput.value;
     const fristNumber = parseFloat(firstText);
@@ -9,10 +10,8 @@ const calculate1 = () => {
     const resultText = text.innerText;
     text.innerText = calculates;
     const result = parseFloat(resultText);
-    console.log(result);
-}
 
-const calculate2 = () => {
+
     const secondInput = document.getElementById('2nd');
     const secondText = secondInput.value;
     const secondNumber = parseFloat(secondText);
@@ -23,10 +22,7 @@ const calculate2 = () => {
     const resultText2 = text2.innerText;
     text2.innerText = calculates2;
     const result2 = parseFloat(resultText2);
-    console.log(result2);
-}
 
-const calculate3 = () => {
     const thirdInput = document.getElementById('3rd');
     const thirdText = thirdInput.value;
     const thirdNumber = parseFloat(thirdText);
@@ -37,10 +33,6 @@ const calculate3 = () => {
     const resultText3 = text3.innerText;
     text3.innerText = calculates3;
     const result3 = parseFloat(resultText3);
-    console.log(result3);
-}
-
-const calculate4 = () => {
     const forthInput = document.getElementById('4th');
     const forthText = forthInput.value;
     const forthNumber = parseFloat(forthText);
@@ -51,10 +43,7 @@ const calculate4 = () => {
     const resultText4 = text4.innerText;
     text4.innerText = calculates4;
     const result4 = parseFloat(resultText4);
-    console.log(result4);
-}
 
-const calculate5 = () => {
     const fivethInput = document.getElementById('5th');
     const fivethText = fivethInput.value;
     const fivethNumber = parseFloat(fivethText);
@@ -65,9 +54,7 @@ const calculate5 = () => {
     const resultText5 = text5.innerText;
     text5.innerText = calculates5;
     const result5 = parseFloat(resultText5);
-    console.log(result5);
-}
-const calculate6 = () => {
+
     const sixthInput = document.getElementById('6th');
     const sixthText = sixthInput.value;
     const sixthNumber = parseFloat(sixthText);
@@ -78,9 +65,8 @@ const calculate6 = () => {
     const resultText6 = text6.innerText;
     text6.innerText = calculates6;
     const result6 = parseFloat(resultText6);
-    console.log(result6);
-}
-const calculate7 = () => {
+
+
     const seventhInput = document.getElementById('7th');
     const seventhText = seventhInput.value;
     const seventhNumber = parseFloat(seventhText);
@@ -91,9 +77,7 @@ const calculate7 = () => {
     const resultText7 = text7.innerText;
     text7.innerText = calculates7;
     const result7 = parseFloat(resultText7);
-    console.log(result7);
-}
-const calculate8 = () => {
+
     const eightInput = document.getElementById('8th');
     const eightText = eightInput.value;
     const eightNumber = parseFloat(eightText);
@@ -104,5 +88,22 @@ const calculate8 = () => {
     const resultText8 = text8.innerText;
     text8.innerText = calculates8;
     const result8 = parseFloat(resultText8);
-    console.log(result8);
-}
+
+    // const totalInput = document.getElementById('total');
+    // const inputText = totalInput.value;
+    // const inputNumber = parseFloat(inputText);
+
+    // const totalAmount = result + result2 + result3 + result4 + result5 + result6 + result7 + result8;
+
+
+    const totalText = document.getElementById('total');
+    const total = totalText.innerText;
+    const totalNumber = parseFloat(total);
+    const totalAmount = result + result2 + result3 + result4 + result5 + result6 + result7 + result8;
+
+    totalText.innerText = totalAmount;
+
+};
+
+
+
